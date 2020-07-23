43e2647b60eb669b0f0507d5a367eb75ead88419
Jose Manuel Minuesa Reyes <nakamadeath@gmail.com>
2020-07-23 19:29:39 +0200

    Level C completed

---
+++ src/main/java/acme/features/entrepreneur/investmentRounds/EntrepreneurInvestmentRoundRepository.java
@@ -0,0 +1,27 @@
+
+package acme.features.entrepreneur.investmentRounds;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.entities.investmentRounds.InvestmentRound;
+import acme.entities.workProgrammes.WorkProgramme;
+import acme.framework.repositories.AbstractRepository;
+
+@Repository
+public interface EntrepreneurInvestmentRoundRepository extends AbstractRepository {
+
+	@Query("select ir from InvestmentRound ir where ir.entrepreneur.id = ?1")
+	Collection<InvestmentRound> findManyByEntrepreneurId(int id);
+
+	@Query("select ir from InvestmentRound ir where ir.id = ?1")
+	InvestmentRound findOneById(int id);
+
+	@Query("select ir from InvestmentRound ir where ir.id = ?1")
+	InvestmentRound findOneInvestmentRoundById(int id);
+
+	@Query("select wp from WorkProgramme wp join wp.investmentRound ir where ir.id = ?1")
+	Collection<WorkProgramme> findWorkProgrammeByInvestmentRoundId(int id);
+}
